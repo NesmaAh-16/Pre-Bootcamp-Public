158c34ebf27f1deaa72a77d42fc2360758c2a48d
NesmaAh-16 <nesmalubbad@example.com>
2026-03-26 22:28:22 +0200

    Adding MakeConnections

--- Web-Fun/JS/MakingConnections/script.js
+++ Web-Fun/JS/MakingConnections/script.js
@@ -8,17 +8,12 @@ var connectionRequests = document.querySelector("#connection-requests");
 var yourConnections = document.querySelector("#your-connections");
 
 function accept(id) {
-    var elem = document.querySelector("#" + id);
-    elem.remove();
-
-    connectionRequests.innerText--;
+    reject(id);
     yourConnections.innerText++;
 }
 
 function reject(id) {
     var elem = document.querySelector("#" + id);
     elem.remove();
-
     connectionRequests.innerText--;
-
 }
